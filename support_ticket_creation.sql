f4215a804c8709ea885683cef8bd4e3588889ece
BSBhuvan <125122426+BSBhuvan@users.noreply.github.com>
2025-06-28 09:35:11 +0530

    Update support_ticket_creation.sql

--- support_ticket_creation.sql
+++ support_ticket_creation.sql
@@ -4,7 +4,7 @@ use support_ticket_system;
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL,
     role ENUM('customer', 'agent', 'admin') DEFAULT 'customer',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
